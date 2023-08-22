a5a62bd626fafe798a1108f6c1d9d0e321a826d4
BigSev15 <palat008@umn.edu>
2023-08-22 16:07:35 -0500

    added readme file

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 30}}> Kirk Cousins GOAT {'\n'}</Text>
+      <Image
+        style={{width: 500, height: 500}} 
+        source={{
+        uri:'https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https://thevikingage.com/wp-content/uploads/getty-images/2016/04/1015328556.jpeg'
+      }}
+      />
+      <Text> {"\n"} Hello World! This is my favorite quarterback!</Text>
       <StatusBar style="auto" />
     </View>
+
+    
   );
 }
 
